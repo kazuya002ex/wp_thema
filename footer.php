bd2a76b656ce75b769df81338d68480a3acaed81
kazuya002ex <chiharu.event2@gmail.com>
2021-01-18 21:31:45 +0900

    feat: header footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer>
+      <p>Copylight©︎ 2021 All Reights Reserved.</p>
+    </footer>
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first thema</title>
+    <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" type="text/css" media="all" />
+    <meta http-equit="X-UA-Compatible" content="IE=edge">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header>
+      <h1>Wordpress!</h1>
+    </header>
